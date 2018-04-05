51f548dbbdeec9dd404aa1756dc9e58621b1ad08
Anton Donskoy <x664@yandex.ru>
2018-04-05 11:25:55 +0300

    test

--- hello.asm
+++ hello.asm
@@ -2,7 +2,7 @@
 ; Writes "Hello, World" to the console using only system calls. Runs on 64-bit macOS only.
 ; To assemble and run:
 ;
-;     nasm -fmacho64 hello.asm && ld hello.o && ./a.out
+;     nasm -fmacho64 hello.asm && ld hello.o && ./a.out 
 ; ----------------------------------------------------------------------------------------
 
           global    start
