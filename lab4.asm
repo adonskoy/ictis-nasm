7e0fdd2e01e31047258cf79e910ccdd56e70dcf7
adonskoy <x664@yandex.ru>
2018-05-29 18:31:56 +0300

    lab4 final

--- lab4.asm
+++ lab4.asm
@@ -6,16 +6,31 @@ global _start ;начало тела программы
 func: 
     cmp byte [char], 10 ;сравнение символа с символом перехода на новую строку 
     je _end 
+
     cmp byte [char], 32 ;сравнение символа с пробелом 
     je _swap 
-    jmp _write 
+    jne inc_len
+    
+    
+inc_len:
+    add esi,1
+    jmp _write
 
 _swap: ;переход на новую строку 
+    cmp byte esi,0
+    jne new_line
+    je _read
+
+
+new_line:
+    mov esi,0
     mov byte [char], 10 
-    ret ;возврат из подпрограммы 
-    section .text 
+    jmp _write
+
+section .text 
 
 _start: 
+    mov esi,0
 _read: ;чтение строки 
     mov eax,3 
     mov ebx,2 
@@ -34,13 +49,14 @@ _write: ;вывод слова на новую строку
 
 _end: 
     ; вывод \n 
-    call _swap
+    mov byte [char], 10 
     mov eax,4 
     mov ebx,1 
     mov ecx,char 
     mov edx,1 
     int 80h 
-    ;конец программы 
-    mov eax,1 
+   
+   
+    mov eax,1 ;конец программы 
     mov ebx,0 
     int 80h
